d21bc50c42b16f0683cce3e5ff45cd41ec539348
martafreri2018 <73704811+martafreri2018@users.noreply.github.com>
2021-02-14 18:01:37 +0100

    Fixed npm

---
+++ src/SearchEngine.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchEngine() {
+  const [city, setCity] = useState("");
+  const [clicked, setClicked] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setClicked(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "329ed4c9b33de1b60e6d184aac65a30a";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (clicked) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}°C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
